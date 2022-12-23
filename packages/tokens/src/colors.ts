89381ac2c50656984d5d88910ba4729c09b255b9
Eduard Dev <eduardkiller@hotmail.com>
2022-12-23 17:39:14 -0300

    Iniciando Rocketseat Design System projeto

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  white: "#fff",
+  black: "#000",
+  gray100: "#E1E1E6",
+  gray200: "#A9A982",
+  gray400: "#7C7C8A",
+  gray500: "#505059",
+  gray600: "#323238",
+  gray700: "#29292E",
+  gray800: "#202024",
+  gray900: "#121214",
+
+  main300: "#00837E",
+  main500: "#00875F",
+  main700: "#015F43",
+  main900: "#00291D"
+}
