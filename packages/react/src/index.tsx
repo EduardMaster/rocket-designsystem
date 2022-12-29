b96b07c61fa79471e3b74b9283d8ddb78bedab97
Eduard Dev <eduardkiller@hotmail.com>
2022-12-29 16:15:14 -0300

    Instalando storybook no monorepo

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -2,8 +2,27 @@ import { styled } from '@stitches/react';
 import React from 'react';
 const Button = styled("button", {
   fontFamily: '$default',
-  backgroundColor: "ActiveText",
-  height: "fit-content"
+  backgroundColor: "$main300",
+  borderRadius: "$sm",
+  border: 0,
+  fontWeight: "bold",
+  color: "White",
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: "$2 $4",
+      },
+      big: {
+        fontSize: 14,
+        padding: "$3 $6"
+      }
+    },
+
+  },
+  defaultVariants: {
+    size: "small"
+  }
 })
 export function App() {
 
